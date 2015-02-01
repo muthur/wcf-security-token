9015e2bc70d4c277ab575abca431d7b516663b0d
muthur <muthunagaswamy@gmail.com>
2015-02-01 14:50:34 -0800

    Initial commit for the WCF service auth example

---
+++ WCFTestService/Account.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCFTestContract;
+using WCFTestService.Filters;
+
+
+
+namespace WCFTestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Account" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Account.svc or Account.svc.cs at the Solution Explorer and start debugging.
+    public class Account : IAccount
+    {
+
+        [Secured]
+        public Money GetAccountBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Secured]
+        public List<Transaction> GetRecentTransactions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WCFTestService/Filters/SecuredAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCFTestService.Filters
+{
+    public class SecuredAttribute : Attribute
+    {
+    }
+}
